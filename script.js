658a313edcd8b6a16c8d6955c99148fe74febbcd
Jonas Barros <jonasfelipester@gmail.com>
2022-05-20 08:29:51 -0300

    Definiçoes no readme

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Sem tamplate String*/
+let nome=Jonas;
+let github = jonasfelipe22;
+let profissão=Jr;
+console.log("Nome : "+nome+ "Niivel "+profissão +"Github"+github);
+
+/* Com tamplate String*/
+console.log(`Nome ${nome} Nivel ${profissão} e o GITHUB ${github}`)
+Operador ternario e arrow functions
+Match Desestruct
